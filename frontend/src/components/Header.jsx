1ee042ad52da6ac3f298a12e980bb62b4c0d33bd
binaryNinjaAbdul <98110639+BinaryNinjaAbdul@users.noreply.github.com>
2023-07-28 15:42:10 +0200

    route & review

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,21 +1,40 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+import { LinkContainer } from 'react-router-bootstrap';
+import logo from '../assets/logo.png';
 
-const Header = () => {
+const header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">WahidShop</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img
+                src={logo}
+                alt="WahidShop"
+                style={{
+                  width: ' 50px',
+                  marginRight: '5px',
+                  transform: `translate(${0}px, ${-5}px)`,
+                }}
+              />
+              WahidShop
+            </Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-avbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
-              <Nav.Link dreh="/cart">
-                <FaShoppingCart /> Cart
-              </Nav.Link>
-              <Nav.Link dreh="/login">
-                <FaUser /> Sign In
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <FaShoppingCart /> Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link>
+                  <FaUser /> Sign In
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
@@ -24,4 +43,4 @@ const Header = () => {
   );
 };
 
-export default Header;
+export default header;
