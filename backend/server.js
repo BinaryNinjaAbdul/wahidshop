670eae519df653929b9e7c1c8e9f58fdb8ff0c27
binaryNinjaAbdul <98110639+BinaryNinjaAbdul@users.noreply.github.com>
2023-08-01 13:03:01 +0200

    Backend Upload file

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+import path from 'path';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 
 // DB config
@@ -25,10 +27,14 @@ app.get('/', (req, res) => {
   res.send('API is running');
 });
 
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
 // Routes
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
 
 app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
